f7143bca0aa716110c6a1b69ace7302c2cb95341
Lee Junmo <ezurno@gmail.com>
2022-09-28 16:53:12 +0900

    JS 기초 및 변수
    1.
     - const 값이 변하지 않음
     - let 값이 변함
     - var는 보호 받지 못하므로 사용 x
    
    2. boolean, null, undefined

---
+++ Project_1/MOMENTUM/script.js
@@ -0,0 +1,32 @@
+console.log("HELLO WORLD . . . !!");
+console.log(120 * 5);
+console.log(120 + 5);
+console.log(120 - 5);
+
+
+// variable(변수) 만들기
+const a = 5;
+const b = "2";
+let myName = "LEE";
+// constant (상수) 값이 변하지 않음
+// let은 값이 변할 수 있음
+// var를 사용할 수 있지만 그럼 value를 보호 받지 못함
+
+console.log(a * b);
+console.log(a + b);
+console.log(a - b);
+
+console.log("Hello! " + myName);
+
+myName = "JM";
+console.log("Hello! " + myName);
+
+
+///////////////////
+
+let TF = false;  // Boolean type
+const not = null; // null type
+let somthing; // undefined type
+
+console.log(TF, not, somthing);
+
