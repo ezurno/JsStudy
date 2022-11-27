c914381788e1fc32afb83db3299a1cfd04fe00e2
Lee Junmo <ezurno@gmail.com>
2022-11-27 14:47:41 +0900

    PROJECT 01
    TODO list

--- Project_1/PROJECT/js/background.js
+++ Project_1/PROJECT/js/background.js
@@ -2,9 +2,11 @@ const imgs = ["bgImg_1.jpg", "bgImg_2.jpg", "bgImg_3.jpg"];
 
 const chosenImg = imgs[Math.floor(Math.random() * imgs.length)];
 
+const background = document.querySelector(".background");
+
 const bgImg = document.createElement("img"); // 변수 만들기
 
 bgImg.src = `img/${chosenImg}`;
 // bgImg의 src를 img/bgImg_1.jpg로 바꿔줌
 
-document.body.appendChild(bgImg); // body에 appendChild로 값 입력
+background.appendChild(bgImg); // body에 appendChild로 값 입력
